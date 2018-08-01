63e6ba2b9aca03e2a7dc780d3efc232c820a0b85
4sight <hgd4th@gmail.com>
2018-08-01 02:20:23 -0400

    introduction of RegEx query for zip codes

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var req = new XMLHttpRequest();
 
 req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=01205a36e129751e14469a7f443b8441', true);
-req.onreadystatechange = function() {
+req.onreadystatechange = function(){
     if (req.readyState === 4) {
-        console.log(req.responseText);
+		data = req.responseText;
+        console.log(data);
     }
-var data = req.responseText;
-// var data = JSON.parse(rawData);
-console.log(data);
 };
 req.setRequestHeader('Accept', 'application/json');
 req.send();
+
+var zipQuery = /^\d/;
+var isNumber = zipQuery.test(8);
+console.log(isNumber);
