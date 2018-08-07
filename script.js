b7b4231e433427c5656f9567f4e7527d9cadc96d
4sight <hgd4th@gmail.com>
2018-08-06 21:15:33 -0400

    calling locations from API

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function userInput(form){
-	var location = form.query.value;
-	console.log(location);
+	var userLocation = form.query.value;
+	var zipQuery = /^\d/;
+	var isNumber = zipQuery.test(userLocation);
+	if (isNumber == true && userLocation.length != 5) {alert('Please enter a location name or a five-digit zip code');}
+	var req = new XMLHttpRequest();
+	req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + userLocation + '&appid=01205a36e129751e14469a7f443b8441', true);
+	req.onreadystatechange = function(){
+	    if (req.readyState === 4) {
+			data = req.responseText;
+	        console.log(data);
+	    }
+	};
+	req.setRequestHeader('Accept', 'application/json');
+	req.send();
 }
-
-var zipQuery = /^\d/;
-var isNumber = zipQuery.test(location);
-console.log(isNumber);
-
-var req = new XMLHttpRequest();
-
-req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=01205a36e129751e14469a7f443b8441', true);
-req.onreadystatechange = function(){
-    if (req.readyState === 4) {
-		data = req.responseText;
-        console.log(data);
-    }
-};
-req.setRequestHeader('Accept', 'application/json');
-req.send();
