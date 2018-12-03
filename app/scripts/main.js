de4e9fe9ac1022caaae006e6b6e7343c6a3091d7
4sight <hgd4th@gmail.com>
2018-12-02 21:22:16 -0500

    gulp setup

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+var d = new Date();
+document.getElementById('demo').innerHTML = d.toString();
+
+function userInput(form){
+	var userLocation = form.locationQuery.value;
+	var zipQuery = /^\d/;
+	var isNumber = zipQuery.test(userLocation);
+	if (isNumber == true && userLocation.length != 5) {alert('Please enter a location name or a five-digit zip code');}
+	var req = new XMLHttpRequest();
+	req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + userLocation + '&appid=01205a36e129751e14469a7f443b8441', true);
+	req.onreadystatechange = function(){
+	    if (req.readyState === 4) {
+			data = req.responseText;
+	        console.log(data);
+	        console.log('The current temperature in ' + userLocation + ' is ' + data.weather + ' °F.');
+	        console.log(data['coord']);
+	    }
+	};
+	req.setRequestHeader('Accept', 'application/json');
+	req.send();
+}
