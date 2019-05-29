067d2a3e5faa92934ae82ff34f078b2d1b925e7d
4sight <hgd4th@gmail.com>
2019-05-29 17:47:22 -0400

    full form outputs temperature

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 function gettingJSON(){
-	var location = document.getElementById('locationQuery').value;
+	var location = document.getElementById('locationQuery').value + ', us';
 	var temp;
 	if (location.length == 5 && /^[0-9]+$/.test(location)) {
 		console.log('Valid zip code')
 	}
-	else { console.log('Not a zip code')}
+	else { console.log('Not a zip code') }
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=01205a36e129751e14469a7f443b8441',function(json){
         console.log(JSON.stringify(json));
         temp = JSON.stringify(json.main.temp);
