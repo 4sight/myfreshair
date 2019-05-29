87eb1c28fbb9ca028d63867933d1ffbc298bba25
4sight <hgd4th@gmail.com>
2019-05-29 17:06:26 -0400

    separating temperature out of JSON results

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,13 @@
 function gettingJSON(){
 	var location = document.getElementById('locationQuery').value;
+	var temp;
 	if (location.length == 5 && /^[0-9]+$/.test(location)) {
 		console.log('Valid zip code')
 	}
 	else { console.log('Not a zip code')}
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=01205a36e129751e14469a7f443b8441',function(json){
         console.log(JSON.stringify(json));
+        temp = JSON.stringify(json.main.temp);
+		console.log(temp);
     });
 }
