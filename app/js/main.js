624ec88f201b048aa1905dcca0e636dc7383e48a
4sight <hgd4th@gmail.com>
2019-05-30 15:30:45 -0400

    pulling forecast and current temperature

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,16 @@
 function gettingJSON(){
 	var location = document.getElementById('locationQuery').value + ', us';
 	var temp;
-	if (location.length == 5 && /^[0-9]+$/.test(location)) {
-		console.log('Valid zip code')
-	}
-	else { console.log('Not a zip code') }
+	// if (location.length == 5 && /^[0-9]+$/.test(location)) {
+	// 	console.log('Valid zip code')
+	// }
+	// else { console.log('Not a zip code') }
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=01205a36e129751e14469a7f443b8441',function(json){
         console.log(JSON.stringify(json));
         temp = JSON.stringify(json.main.temp);
 		console.log(temp);
     });
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=01205a36e129751e14469a7f443b8441',function(json){
+    	console.log(JSON.stringify(json));
+    })
 }
