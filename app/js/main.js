3e815844440d2a5add5852e1441f8f00f84ba427
4sight <hgd4th@gmail.com>
2019-06-03 15:37:55 -0400

    loop stops at desired forecast ID

--- app/js/main.js
+++ app/js/main.js
@@ -11,30 +11,49 @@ function gettingJSON(){
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=01205a36e129751e14469a7f443b8441',function(json){
         console.log(JSON.stringify(json));
         temp = JSON.stringify(json.main.temp);
-		console.log(temp);
+		console.log('The current temperature is ' + temp + '°K');
     });
     $.getJSON('http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=01205a36e129751e14469a7f443b8441',function(json){
-    	// for (i = 0; i < JSON.stringify(json.length); )
     	forecast = json.list;
+        var forecastID = '';
     	console.log(Object.keys(forecast).length);
         if (heat.checked) {
-            if (temp > desiredTemp) {
+            if (desiredTemp === '') {
+                window.alert('Please enter your desired temperature.')
+            } else if (temp > desiredTemp) {
                 window.alert('Open your windows now!')
             } else if (temp < desiredTemp) {
-                for (i = 0; i < Object.keys(forecast).length; i++) {
-                    
+                for (i = 0; forecast[i].main.temp < desiredTemp; i++) {
+                    console.log(i);
+                    console.log(forecast[i].dt);
+                    console.log(forecast[i].main.temp);
+                    // window.alert('Open your windows at');
                 }       
-                console.log(forecast[i])
-                console.log(forecast[i].main.temp);
             }
         } else if (cool.checked) {
-
+            if (desiredTemp === '') {
+                window.alert('Please enter your desired temperature.')
+            } else if (temp < desiredTemp) {
+                window.alert('Open your windows now!')
+            } else if (temp > desiredTemp) {
+                for (i = 0; forecast[i].main.temp > desiredTemp; i++) {
+                    console.log(i);
+                    console.log(forecast[i].dt);
+                    console.log(forecast[i].main.temp);
+                    console.log(i + 1);
+                    console.log(forecast[i + 1].dt);
+                    console.log(forecast[i + 1].main.temp);
+                    // window.alert('Open your windows at');
+                    forecastID = i;
+                }
+                console.log(forecastID);     
+            }
         }
         else {
-            window.alert('Please select one of the two heating or cooling options.')
+            window.alert('Please select either the heat up or cool down option.')
         }
     })
     var time = (new Date).getTime();
     console.log(time / 1000);
-
+    // (9 / 5 * (JSON.stringify(json.main.temp) - 273.15) + 32).toPrecision(5);
 }
